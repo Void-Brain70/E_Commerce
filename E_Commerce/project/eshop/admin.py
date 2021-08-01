49732127fe97a2fb42dcc085fd9ac77001cc7745
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2021-08-01 12:22:49 +0600

    contact page work

--- E_Commerce/project/eshop/admin.py
+++ E_Commerce/project/eshop/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Contact
 # Register your models here.
 @admin.register(Product)
 class Product(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
     pass
